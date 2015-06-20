1eb508fdfc5de2ff9989678942cb1d951cbaaa1b
venu.kandagatla@gmail.com <venu.kandagatla@gmail.com>
2015-06-20 09:02:00 +0530

    Random quote api added

--- server.js
+++ server.js
@@ -2,13 +2,35 @@ var express = require('express');
 
 var app = express();
 
+var quotes = [
+  { author : 'Audrey Hepburn', text : "Nothing is impossible, the word itself says 'I'm possible'!"},
+  { author : 'Walt Disney', text : "You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you"},
+  { author : 'Unknown', text : "Even the greatest was once a beginner. Don't be afraid to take that first step."},
+  { author : 'Neale Donald Walsch', text : "You are afraid to die, and you're afraid to live. What a way to exist."}
+];
+
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+//adding route for getting random quote
+router.route('/quote/random')
+  .get(function(req, res){
+    var id = Math.floor(Math.random() * quotes.length);
+    var q = quotes[id];
+    res.json(q);
 });
 
 var port = process.env.PORT || 8000;        // set our port
